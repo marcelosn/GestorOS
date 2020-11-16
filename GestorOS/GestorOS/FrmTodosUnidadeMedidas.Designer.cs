24176ac0f4a796ad7760d856b69390b8fca235cb
Luís Antônio Oliveira Maia <luisantoniopvh@hotmail.com>
2020-11-15 20:42:31 -0400

    a

--- GestorOS/GestorOS/FrmTodosUnidadeMedidas.Designer.cs
+++ GestorOS/GestorOS/FrmTodosUnidadeMedidas.Designer.cs
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmTodosUnidadeMedidas));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.lwUnidades = new System.Windows.Forms.ListView();
+            this.columnHeaderCodigo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderUnidade = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSigla = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnNovo);
+            this.panel1.Controls.Add(this.lwUnidades);
+            this.panel1.Controls.Add(this.txtBuscar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 444);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.Image = ((System.Drawing.Image)(resources.GetObject("btnNovo.Image")));
+            this.btnNovo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNovo.Location = new System.Drawing.Point(584, 10);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(181, 32);
+            this.btnNovo.TabIndex = 14;
+            this.btnNovo.Text = "Adicionar novo - F1";
+            this.btnNovo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // lwUnidades
+            // 
+            this.lwUnidades.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lwUnidades.BackColor = System.Drawing.Color.SeaShell;
+            this.lwUnidades.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lwUnidades.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderCodigo,
+            this.columnHeaderUnidade,
+            this.columnHeaderSigla});
+            this.lwUnidades.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lwUnidades.FullRowSelect = true;
+            this.lwUnidades.GridLines = true;
+            this.lwUnidades.Location = new System.Drawing.Point(9, 51);
+            this.lwUnidades.Name = "lwUnidades";
+            this.lwUnidades.Size = new System.Drawing.Size(756, 379);
+            this.lwUnidades.TabIndex = 13;
+            this.lwUnidades.UseCompatibleStateImageBehavior = false;
+            this.lwUnidades.View = System.Windows.Forms.View.Details;
+            this.lwUnidades.DoubleClick += new System.EventHandler(this.lwUnidades_DoubleClick);
+            // 
+            // columnHeaderCodigo
+            // 
+            this.columnHeaderCodigo.Text = "Código";
+            this.columnHeaderCodigo.Width = 64;
+            // 
+            // columnHeaderUnidade
+            // 
+            this.columnHeaderUnidade.Text = "Descrição";
+            this.columnHeaderUnidade.Width = 379;
+            // 
+            // columnHeaderSigla
+            // 
+            this.columnHeaderSigla.Text = "Sigla";
+            this.columnHeaderSigla.Width = 163;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBuscar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(9, 9);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(569, 33);
+            this.txtBuscar.TabIndex = 0;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // FrmTodosUnidadeMedidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmTodosUnidadeMedidas";
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "FrmTodosUnidadeMedidas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Relação de unidades de medidas";
+            this.Load += new System.EventHandler(this.FrmTodosUnidadeMedidas_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmTodosUnidadeMedidas_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.ListView lwUnidades;
+        private System.Windows.Forms.ColumnHeader columnHeaderCodigo;
+        private System.Windows.Forms.ColumnHeader columnHeaderUnidade;
+        private System.Windows.Forms.ColumnHeader columnHeaderSigla;
+        private System.Windows.Forms.TextBox txtBuscar;
     }
 }
--- GestorOS/GestorOS/FrmTodosUnidadeMedidas.cs
+++ GestorOS/GestorOS/FrmTodosUnidadeMedidas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GestorOS.Data;
+using GestorOS.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,73 @@ namespace GestorOS
 {
     public partial class FrmTodosUnidadeMedidas : Form
     {
+        MeuDataContext meuDataContext;
+        public UnidadeMedida unidadeMedida;
+
         public FrmTodosUnidadeMedidas()
         {
             InitializeComponent();
+            meuDataContext = new MeuDataContext();
+        }
+
+        private void FrmTodosUnidadeMedidas_Load(object sender, EventArgs e)
+        {
+            txtBuscar.Focus();
+            CarregaTodasUnidades();
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CarregaTodasUnidades()
+        {
+            var unidades = meuDataContext.UnidadeMedidas
+                                         .AsNoTracking()
+                                         .Where(u => u.Nome.Contains(txtBuscar.Text) || u.Sigla.Contains(txtBuscar.Text)).OrderBy(u=> u.Nome).ToList();
+            if (unidades.Count > 0)
+            {
+                lwUnidades.Items.Clear();
+                int item = 0;
+
+                foreach (var unidade in unidades)
+                {
+                    lwUnidades.Items.Add(unidade.Id.ToString());
+                    lwUnidades.Items[item].SubItems.Add(unidade.Nome);
+                    lwUnidades.Items[item].SubItems.Add(unidade.Sigla);
+                    item++;
+                }
+            }
+        }
+
+        private void FrmTodosUnidadeMedidas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            else if (e.KeyCode == Keys.F1)
+            {
+                btnNovo_Click(this, e);
+            }
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            CarregaTodasUnidades();
+        }
+
+        private void lwUnidades_DoubleClick(object sender, EventArgs e)
+        {
+            if (lwUnidades.Items.Count > 0)
+            {
+                int unidadeId = int.Parse(lwUnidades.FocusedItem.SubItems[0].Text);
+
+                unidadeMedida = meuDataContext.UnidadeMedidas.Find(unidadeId);
+
+                this.Close();
+            }
         }
     }
 }
